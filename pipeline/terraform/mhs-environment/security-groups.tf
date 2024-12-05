309c5abd9613d4e378b350152e5b6dc0719af3a8
Mike Wong <mike.wong@blinxsolutions.com>
2024-12-05 00:22:09 +0000

    remove 80

--- pipeline/terraform/mhs-environment/security-groups.tf
+++ pipeline/terraform/mhs-environment/security-groups.tf
@@ -411,13 +411,6 @@ resource "aws_security_group" "alb_outbound_security_group" {
     description = "Allow inbound HTTPS connections from supplier VPC"
   }
 
-  ingress {
-    from_port = 80 
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Tmp rule to allow HTTP traffic from the internet"
-  }
 
   ingress {
     from_port = 443
